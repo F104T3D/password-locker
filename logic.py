a4183bdcda90fc5cedc13e0306e292ebef2d2ce7
F104T3D <f104t3d@proton.me>
2025-06-17 19:14:47 +1000

    new funcs - keygen encryption

--- logic.py
+++ logic.py
@@ -11,10 +11,40 @@ def add_password(site, username, password):
             json.dump(data, file, indent=4)
 
 def view_password():
-    print("[1] All")
     choice = int(input("Choose an option (1-5): ").strip())
 
     if choice == 1:
         with open("passwords.json", "r") as file:
             data = json.load(file)
             print(data)
+
+def search_password(site):
+    with open("passwords.json", "r") as file:
+        data = json.load(file)
+        for entry in data:
+            if entry["site"] == site:
+                print(f"Site: {entry['site']}, Username: {entry['username']}, Password: {entry['password']}")
+                return
+        print("No entry found for that site.")
+
+def delete_password(site):
+    if not site:
+        print("Site cannot be empty.")
+        return
+    with open("passwords.json", "r") as file:
+        data = json.load(file)
+    
+    new_data = [entry for entry in data if entry["site"] != site]
+    
+    if len(new_data) < len(data):
+        with open("passwords.json", "w") as file:
+            json.dump(new_data, file, indent=4)
+        print(f"Entry for {site} deleted.")
+    else:
+        print(f"No entry found for {site}.")
+def ensure_file_exists():
+    try:
+        with open("passwords.json", "x") as file:
+            json.dump([], file, indent=4)
+    except FileExistsError:
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import logic
 
+try:
+    with open("keygen/key.key", "rb") as key_file:
+        key = key_file.read()
+        print("[+] Key found. Proceeding with the program...")
+except FileNotFoundError:
+    from keygen import generator
+    print("[!] Key not found. Generating a new key...")
+    generator.generate_key()
+    print("[+] Key generated successfully. Restarting the program.")
+    exit()
+
 while True:
     print("[1] Add new password\n[2] View all entries \n[3] Search by site\n[4] Delete an entry\n[5] Exit")
     
@@ -15,11 +26,14 @@ while True:
         pword = input("password: ").strip()
         logic.add_password(site, user, pword)
     elif choice == 2:
+        print("[1] All")
         logic.view_password()
     elif choice == 3:
-        print("holder")
+        siteinp = input("site: ")
+        logic.search_password(siteinp)
     elif choice == 4:
-        print("holder")
+        siteinp = input("site: ")
+        logic.delete_password(siteinp)
     elif choice == 5:
         print("Goodbye.")
         break
